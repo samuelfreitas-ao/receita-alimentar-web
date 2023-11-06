ecee2048cc19b7d15da7b2b158927d25b56b3625
Samuel Freitas <samuelfreitas.ao@gmail.com>
2023-11-06 16:22:54 +0000

    feat: generate pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './presentation/pages/home/home.component';
+import { LoginComponent } from './presentation/pages/login/login.component';
+import { LayoutComponent } from './presentation/components/layout/layout.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  {
+    path: '',
+    component: LayoutComponent,
+    children: [{ path: '', component: HomeComponent }],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './presentation/components/header/header.component';
+import { HomeComponent } from './presentation/pages/home/home.component';
+import { FooterComponent } from './presentation/components/footer/footer.component';
+import { LoginComponent } from './presentation/pages/login/login.component';
+import { LayoutComponent } from './presentation/components/layout/layout.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    FooterComponent,
+    LoginComponent,
+    LayoutComponent
   ],
   imports: [
     BrowserModule,
